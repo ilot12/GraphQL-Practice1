2d1d6fc14a025b9ac1c615e63cac2321c65b641c
ilot12 <rlaehdwn60@gmail.com>
2024-01-09 13:47:38 +0900

    feat: #4.1까지 수강

---
+++ server.js
@@ -0,0 +1,17 @@
+import { ApolloServer, gql } from "apollo-server";
+
+//위와 아래는 같은 의미이지만 
+//type:module라는 것을 package.json에서 추가해서 위와 같이 쓸 수 있음.
+//const {ApolloServer, gql} = require("apollo-server");
+
+const typeDefs = gql`
+
+
+
+`
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(({url}) => {
+  console.log(`Running on ${url}`);
+});
