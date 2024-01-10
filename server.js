a28f39afd1b1b602c36078f39af28ebf1be10c8b
ilot12 <rlaehdwn60@gmail.com>
2024-01-10 14:32:28 +0900

    feat: ~#4.7 mutation Resolvers

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ import { ApolloServer, gql } from "apollo-server";
 //type:module라는 것을 package.json에서 추가해서 위와 같이 쓸 수 있음.
 //const {ApolloServer, gql} = require("apollo-server");
 
+let tweets = [
+  {
+    id: "1",
+    text: "first one",
+  },
+  {
+    id: "2",
+    text: "second one",
+  }
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -12,11 +23,11 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
@@ -24,7 +35,34 @@ const typeDefs = gql`
   }
 `
 
-const server = new ApolloServer({typeDefs});
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    }
+  },
+  Mutation: {
+    postTweet(root, {text, userId}) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      }
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(root, { id }) {
+      const tweet = tweets.find(tweet => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter(tweet => tweet.id !== id);
+      return true;
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({url}) => {
   console.log(`Running on ${url}`);
